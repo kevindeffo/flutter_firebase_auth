1a6b3a439950690d6e967df8f8e94a8d18de25ee
kevindeffo <deffokevin14@gmail.com>
2023-05-06 15:55:15 +0100

    end of login form

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -17,13 +17,38 @@ class AuthGate extends StatelessWidget {
            providerConfigs: const [
              EmailProviderConfiguration(),
            ],
-           headerBuilder: (context, constraints, shrinkOffset
-) {
+           headerBuilder: (context, constraints, shrinkOffset) {
              return Padding(
                padding: const EdgeInsets.all(20),
                child: AspectRatio(
                  aspectRatio: 1,
                  child: Image.asset('assets/flutterfire_300x.png'),
+               ),
+             );
+           },
+           subtitleBuilder: (context, action) {
+             return Padding(
+               padding: const EdgeInsets.symmetric(vertical: 8.0),
+               child: action == AuthAction.signIn
+                   ? const Text('Welcome to FlutterFire, please sign in!')
+                   : const Text('Welcome to Flutterfire, please sign up!'),
+             );
+           },
+           footerBuilder: (context, action) {
+             return const Padding(
+               padding: EdgeInsets.only(top: 16),
+               child: Text(
+                 'By signing in, you agree to our terms and conditions.',
+                 style: TextStyle(color: Colors.grey),
+               ),
+             );
+           },
+           sideBuilder: (context, shrinkOffset) {
+             return Padding(
+               padding: const EdgeInsets.all(20),
+               child: AspectRatio(
+                 aspectRatio: 1,
+                 child: Image.asset('flutterfire_300x.png'),
                ),
              );
            },
