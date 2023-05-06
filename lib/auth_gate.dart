28a4fe63c932b6383b24252231b2e5fce908525f
kevindeffo <deffokevin14@gmail.com>
2023-05-06 15:27:25 +0100

    login form

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,12 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
 
 import 'home.dart';
 
 class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
+ const AuthGate({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const HomeScreen();
-  }
+ @override
+ Widget build(BuildContext context) {
+   return StreamBuilder<User?>(
+     stream: FirebaseAuth.instance.authStateChanges(),
+     builder: (context, snapshot) {
+       if (!snapshot.hasData) {
+         return SignInScreen(
+           providerConfigs: const [
+             EmailProviderConfiguration(),
+           ],
+           headerBuilder: (context, constraints, shrinkOffset
+) {
+             return Padding(
+               padding: const EdgeInsets.all(20),
+               child: AspectRatio(
+                 aspectRatio: 1,
+                 child: Image.asset('assets/flutterfire_300x.png'),
+               ),
+             );
+           },
+         );
+       }
+
+       return const HomeScreen();
+     },
+   );
+ }
 }
